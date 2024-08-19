6bac098ea171ff330c2f8d2d30c4c5ac44d6b6af
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2024-08-19 17:25:43 +0300

    create footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,113 @@
 <footer class="footer">
     <div class="container">
         <div class="footer-inner">
-            123
+            <div class="row">
+                <div class="col-12 col-md-6 col-xl-auto">
+                    <div class="footer-start">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="block">
+                                    <div class="block-title">
+                                        <h4 class="h-4">
+                                            Читай с детьми
+                                        </h4>
+                                    </div>
+                                    <div class="block-content">
+                                        <p class="text-1">
+                                            Всероссийский конкурс социальных проектов
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="block block-contacts">
+                                    <div class="block-title">
+                                        <h4 class="h-4">
+                                            Свяжитесь с нами
+                                        </h4>
+                                    </div>
+                                    <div class="block-content">
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <a href="tel:+7(999)999-99-99" class="text-3 fw-300">+7(999)999-99-99</a>
+                                            </div>
+                                            <div class="col-12">
+                                                <a href="mailto:email@gmail.com" class="text-3 fw-300">email@gmail.com</a>
+                                            </div>
+                                            <div class="col-12">
+                                                <span class="text-3 fw-300">
+                                                    Москва, Краснопресненская набережная, д. 12
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-xl">
+                    <div class="footer-middle">
+                        <form class="form" action="?">
+                            @csrf
+                            <div class="row">
+                                <div class="col-12">
+                                    <p>
+                                        Получайте сообщения о каждом обновлении, которое мы публикуем.
+                                    </p>
+                                    <p>
+                                        Получайте сообщения о каждом обновлении, которое мы публикуем.
+                                    </p>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-label form-relative">
+                                        <input name="email" type="text" class="form-control" rows="1" value="" id="email" placeholder="Введите ваш e-mail" />
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <button type="submit" class="btn btn-icon btn-primary">
+                                        <span class="btn-icon-text">
+                                            подписаться
+                                        </span>
+                                        <svg fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
+                                            <use xlink:href="/sprite.svg#arrowRight"></use>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-xl-auto">
+                    <div class="footer-end">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="footer-social">
+                                    <div class="row">
+                                        <div class="col-auto"></div>
+                                        <div class="col-auto"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="footer-copyright">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <span>© 2024 Все права защищены.</span>
+                                        </div>
+                                        <div class="col-12">
+                                            <a href="javascript://" target="_blank">Политика конфиденциальности</a>
+                                        </div>
+                                        <div class="col-12">
+                                            <a href="javascript://" target="_blank">Сезон 2024</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </footer>
