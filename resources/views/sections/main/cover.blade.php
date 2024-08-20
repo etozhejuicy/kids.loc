9fb3230a24aacff51118a66ebed52a336c562e72
serj <zenitovic@gmail.com>
2024-08-20 11:14:26 +0300

    section cover update

--- resources/views/sections/main/cover.blade.php
+++ resources/views/sections/main/cover.blade.php
@@ -1,18 +1,24 @@
 <section class="section section-content section-cover">
-    <div class="section-cover-background">
-        <img data-src="/assets/images/pages/main/sections/cover/bg.png" class="lazyload" />
-    </div>
     <div class="section-cover-inner">
+        <div class="section-cover-background">
+            <img data-src="/assets/images/pages/main/sections/cover/bg.png" class="lazyload" />
+        </div>
         <div class="section-cover-content">
             <div class="container">
-                <div class="section-cover-title">
-                    Добро пожаловать в мир книг вместе с проектом «Читай с детьми»! 📚
-                </div>
-                <div class="section-cover-title">
-                    Добро пожаловать в мир книг вместе с проектом «Читай с детьми»! 📚
-                </div>
-                <div class="section-cover-title">
-                    Добро пожаловать в мир книг вместе с проектом «Читай с детьми»! 📚
+                <div class="section-cover-context">
+                    <div class="section-cover-title">
+                        Добро пожаловать в мир <br class="d-inline d-xl-none">
+                        книг вместе <br class="d-none d-xl-inline">с проектом <br class="d-inline d-xl-none">
+                        «Читай с детьми»! 📚
+                    </div>
+                    <div class="section-cover-next">
+                        <div class="section-cover-arrow">
+
+                        </div>
+                        <div class="section-cover-subtitle">
+                            Цифры проекта
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
