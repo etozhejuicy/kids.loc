32cbdda25afb9d8c9bc4515c85c4c13c4e7d4956
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2024-08-20 23:46:18 +0300

    create section social

---
+++ resources/views/sections/main/social.blade.php
@@ -0,0 +1,56 @@
+<section class="section section-content section-social">
+    <div class="container">
+        <div class="section-social-inner">
+            <div class="section-social-block">
+                <div class="section-social-title text-left text-md-center">
+                    <h3 class="h-1 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0s">
+                        Наши социальные сети
+                    </h3>
+                </div>
+                <div class="section-social-list">
+                    <div class="row">
+                        <div class="col-12 col-md-6 col-xl-4">
+                            <div class="social-card">
+                                <div class="social-card-inner">
+                                    <div class="social-card-image">
+                                        <img data-src="/assets/images/pages/main/sections/social/rutube.svg" class="lazyload" />
+                                    </div>
+                                    <div class="social-card-title">
+                                        Рутуб
+                                    </div>
+                                </div>
+                                <a href="javascript://" target="_blank" class="stretched-link"></a>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 col-xl-4">
+                            <div class="social-card">
+                                <div class="social-card-inner">
+                                    <div class="social-card-image">
+                                        <img data-src="/assets/images/pages/main/sections/social/vk.svg" class="lazyload" />
+                                    </div>
+                                    <div class="social-card-title">
+                                        Вконтакте
+                                    </div>
+                                </div>
+                                <a href="javascript://" target="_blank" class="stretched-link"></a>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 col-xl-4">
+                            <div class="social-card">
+                                <div class="social-card-inner">
+                                    <div class="social-card-image">
+                                        <img data-src="/assets/images/pages/main/sections/social/tg.svg" class="lazyload" />
+                                    </div>
+                                    <div class="social-card-title">
+                                        Телеграм
+                                    </div>
+                                </div>
+                                <a href="javascript://" target="_blank" class="stretched-link"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
