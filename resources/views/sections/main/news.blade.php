05b92c923be8f96718594089f4b2c34d0012c84b
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2024-08-20 23:03:57 +0300

    create section-news

---
+++ resources/views/sections/main/news.blade.php
@@ -0,0 +1,100 @@
+<section class="section section-content section-news">
+    <div class="container">
+        <div class="section-news-inner">
+            <div class="section-news-title text-left text-md-center">
+                <h3 class="h-1 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0s">
+                    Новости
+                </h3>
+            </div>
+            <div class="section-news-content">
+                <div class="row">
+                    <div class="col-12 col-md-6">
+                        <div class="news-card">
+                            <div class="news-card-inner">
+                                <div class="news-card-cover">
+                                    <img data-src="" class="lazyload" />
+                                </div>
+                                <div class="news-card-content">
+                                    <div class="news-card-title">
+                                        Lorem ipsum dolor sit amet
+                                    </div>
+                                    <div class="news-card-description">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                    </div>
+                                    <div class="news-card-link">
+                                        <a href="javascript://" target="_blank">Читать</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <div class="news-card">
+                            <div class="news-card-inner">
+                                <div class="news-card-cover">
+                                    <img data-src="" class="lazyload" />
+                                </div>
+                                <div class="news-card-content">
+                                    <div class="news-card-title">
+                                        Lorem ipsum dolor sit amet
+                                    </div>
+                                    <div class="news-card-description">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                    </div>
+                                    <div class="news-card-link">
+                                        <a href="javascript://" target="_blank">Читать</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <div class="news-card">
+                            <div class="news-card-inner">
+                                <div class="news-card-cover">
+                                    <img data-src="" class="lazyload" />
+                                </div>
+                                <div class="news-card-content">
+                                    <div class="news-card-title">
+                                        Lorem ipsum dolor sit amet
+                                    </div>
+                                    <div class="news-card-description">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                    </div>
+                                    <div class="news-card-link">
+                                        <a href="javascript://" target="_blank">Читать</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <div class="news-card">
+                            <div class="news-card-inner">
+                                <div class="news-card-cover">
+                                    <img data-src="" class="lazyload" />
+                                </div>
+                                <div class="news-card-content">
+                                    <div class="news-card-title">
+                                        Lorem ipsum dolor sit amet
+                                    </div>
+                                    <div class="news-card-description">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                    </div>
+                                    <div class="news-card-link">
+                                        <a href="javascript://" target="_blank">Читать</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section-news-action">
+                <button type="button" class="btn btn-white btn-wider">
+                    Загрузить еще
+                </button>
+            </div>
+        </div>
+    </div>
+</section>
