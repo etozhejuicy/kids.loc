648e52db61902497ad0d1d5fc76ef68d84bd4426
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2024-08-20 22:27:38 +0300

    create section video

--- resources/views/sections/main/graphic.blade.php
+++ resources/views/sections/main/graphic.blade.php
@@ -1,7 +1,7 @@
 <section class="section section-content section-graphic">
     <div class="container">
         <div class="section-graphic-inner">
-            <div class="section-graphic-title">
+            <div class="section-graphic-title text-left text-md-center">
                 <h3 class="h-1 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0s">
                     График проекта
                 </h3>
---
+++ resources/views/sections/main/video.blade.php
@@ -0,0 +1,192 @@
+<section class="section section-content section-video">
+    <div class="container">
+        <div class="section-video-inner">
+            <div class="section-video-title text-left text-md-center">
+                <h3 class="h-1 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0s">
+                    <span class="d-block d-lg-none">
+                        Видео
+                    </span>
+                    <span class="d-none d-lg-block">
+                        Мастер-классы по созданию книжного <br>
+                        контента для детей и родителей
+                    </span>
+                </h3>
+            </div>
+            <div class="section-video-content">
+                <div class="row">
+                    <div class="col-12 col-md-6 col-xl-4">
+                        <div class="video-card wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0s">
+                            <div class="video-card-inner">
+                                <div class="video-card-cover">
+                                    <img data-src="/assets/images/pages/main/sections/video/example.svg" class="lazyload" />
+                                </div>
+                                <div class="video-card-content">
+                                    <div class="video-card-author">
+                                        Игорь М. Намаконов
+                                    </div>
+                                    <div class="video-card-description">
+                                        Что такое энергия неведения?
+                                        Что такое энергия неведения?
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="javascript://" class="stretched-link"></a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-xl-4">
+                        <div class="video-card wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0s">
+                            <div class="video-card-inner">
+                                <div class="video-card-cover">
+                                    <img data-src="/assets/images/pages/main/sections/video/example.svg" class="lazyload" />
+                                </div>
+                                <div class="video-card-content">
+                                    <div class="video-card-author">
+                                        Игорь М. Намаконов
+                                    </div>
+                                    <div class="video-card-description">
+                                        Что такое энергия неведения?
+                                        Что такое энергия неведения?
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="javascript://" class="stretched-link"></a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-xl-4">
+                        <div class="video-card wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0s">
+                            <div class="video-card-inner">
+                                <div class="video-card-cover">
+                                    <img data-src="/assets/images/pages/main/sections/video/example.svg" class="lazyload" />
+                                </div>
+                                <div class="video-card-content">
+                                    <div class="video-card-author">
+                                        Игорь М. Намаконов
+                                    </div>
+                                    <div class="video-card-description">
+                                        Что такое энергия неведения?
+                                        Что такое энергия неведения?
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="javascript://" class="stretched-link"></a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-xl-4">
+                        <div class="video-card wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0s">
+                            <div class="video-card-inner">
+                                <div class="video-card-cover">
+                                    <img data-src="/assets/images/pages/main/sections/video/example.svg" class="lazyload" />
+                                </div>
+                                <div class="video-card-content">
+                                    <div class="video-card-author">
+                                        Игорь М. Намаконов
+                                    </div>
+                                    <div class="video-card-description">
+                                        Что такое энергия неведения?
+                                        Что такое энергия неведения?
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="javascript://" class="stretched-link"></a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-xl-4">
+                        <div class="video-card wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0s">
+                            <div class="video-card-inner">
+                                <div class="video-card-cover">
+                                    <img data-src="/assets/images/pages/main/sections/video/example.svg" class="lazyload" />
+                                </div>
+                                <div class="video-card-content">
+                                    <div class="video-card-author">
+                                        Игорь М. Намаконов
+                                    </div>
+                                    <div class="video-card-description">
+                                        Что такое энергия неведения?
+                                        Что такое энергия неведения?
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="javascript://" class="stretched-link"></a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-xl-4">
+                        <div class="video-card wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0s">
+                            <div class="video-card-inner">
+                                <div class="video-card-cover">
+                                    <img data-src="/assets/images/pages/main/sections/video/example.svg" class="lazyload" />
+                                </div>
+                                <div class="video-card-content">
+                                    <div class="video-card-author">
+                                        Игорь М. Намаконов
+                                    </div>
+                                    <div class="video-card-description">
+                                        Что такое энергия неведения?
+                                        Что такое энергия неведения?
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="javascript://" class="stretched-link"></a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-xl-4">
+                        <div class="video-card wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0s">
+                            <div class="video-card-inner">
+                                <div class="video-card-cover">
+                                    <img data-src="/assets/images/pages/main/sections/video/example.svg" class="lazyload" />
+                                </div>
+                                <div class="video-card-content">
+                                    <div class="video-card-author">
+                                        Игорь М. Намаконов
+                                    </div>
+                                    <div class="video-card-description">
+                                        Что такое энергия неведения?
+                                        Что такое энергия неведения?
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="javascript://" class="stretched-link"></a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-xl-4">
+                        <div class="video-card wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0s">
+                            <div class="video-card-inner">
+                                <div class="video-card-cover">
+                                    <img data-src="/assets/images/pages/main/sections/video/example.svg" class="lazyload" />
+                                </div>
+                                <div class="video-card-content">
+                                    <div class="video-card-author">
+                                        Игорь М. Намаконов
+                                    </div>
+                                    <div class="video-card-description">
+                                        Что такое энергия неведения?
+                                        Что такое энергия неведения?
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="javascript://" class="stretched-link"></a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-xl-4">
+                        <div class="video-card wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0s">
+                            <div class="video-card-inner">
+                                <div class="video-card-cover">
+                                    <img data-src="/assets/images/pages/main/sections/video/example.svg" class="lazyload" />
+                                </div>
+                                <div class="video-card-content">
+                                    <div class="video-card-author">
+                                        Игорь М. Намаконов
+                                    </div>
+                                    <div class="video-card-description">
+                                        Что такое энергия неведения?
+                                        Что такое энергия неведения?
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="javascript://" class="stretched-link"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
