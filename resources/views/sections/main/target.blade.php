6d7fd3c96b876acde67be769f013337f1c66ba47
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2024-08-20 13:07:39 +0300

    create target section

---
+++ resources/views/sections/main/target.blade.php
@@ -0,0 +1,23 @@
+<section class="section section-content section-target">
+    <div class="container">
+        <div class="section-target-inner">
+            <div class="section-target-title">
+                <h4 class="h-4">
+                    Наши цели
+                </h4>
+            </div>
+            <div class="section-target-content">
+                <div class="section-target-background">
+                    <img data-src="/assets/images/pages/main/sections/target/bg.svg" class="lazyload" />
+                </div>
+                <div class="section-target-list">
+                    <ul>
+                        <li>Разжигать любовь к чтению у детей и подростков, сделать книги важной частью их жизни;</li>
+                        <li>Поддерживать родителей в создании книжных традиций в семье;</li>
+                        <li>Развивать профессиональные навыки педагогов и представителей культурного сообщества через специализированные программы и тренинги.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
