db26930599ebc81d8ef4c5d7254eee6b0cf8f462
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2024-08-20 23:35:58 +0300

    create section partners

---
+++ resources/views/sections/main/partners.blade.php
@@ -0,0 +1,128 @@
+<section class="section section-content section-partners">
+    <div class="container">
+        <div class="section-partners-inner">
+            <div class="section-partners-block">
+                <div class="section-partners-title text-left text-md-center">
+                    <h3 class="h-1 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0s">
+                        Организаторы
+                    </h3>
+                </div>
+                <div class="section-partners-list">
+                    <div class="row">
+                        <div class="col-12 col-md-6 col-xl-4">
+                            <div class="partner-card">
+                                <div class="partner-card-inner">
+                                    <div class="partner-card-image">
+                                        <img data-src="/assets/images/pages/main/sections/partners/example.svg" class="lazyload" />
+                                    </div>
+                                </div>
+                                <a href="javascript://" target="_blank" class="stretched-link"></a>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 col-xl-4">
+                            <div class="partner-card">
+                                <div class="partner-card-inner">
+                                    <div class="partner-card-image">
+                                        <img data-src="/assets/images/pages/main/sections/partners/example.svg" class="lazyload" />
+                                    </div>
+                                </div>
+                                <a href="javascript://" target="_blank" class="stretched-link"></a>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 col-xl-4">
+                            <div class="partner-card">
+                                <div class="partner-card-inner">
+                                    <div class="partner-card-image">
+                                        <img data-src="/assets/images/pages/main/sections/partners/example.svg" class="lazyload" />
+                                    </div>
+                                </div>
+                                <a href="javascript://" target="_blank" class="stretched-link"></a>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 col-xl-4">
+                            <div class="partner-card">
+                                <div class="partner-card-inner">
+                                    <div class="partner-card-image">
+                                        <img data-src="/assets/images/pages/main/sections/partners/example.svg" class="lazyload" />
+                                    </div>
+                                </div>
+                                <a href="javascript://" target="_blank" class="stretched-link"></a>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 col-xl-4">
+                            <div class="partner-card">
+                                <div class="partner-card-inner">
+                                    <div class="partner-card-image">
+                                        <img data-src="/assets/images/pages/main/sections/partners/example.svg" class="lazyload" />
+                                    </div>
+                                </div>
+                                <a href="javascript://" target="_blank" class="stretched-link"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section-partners-block">
+                <div class="section-partners-title text-left text-md-center">
+                    <h3 class="h-1 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0s">
+                        Партнеры
+                    </h3>
+                </div>
+                <div class="section-partners-list">
+                    <div class="row">
+                        <div class="col-12 col-md-6 col-xl-4">
+                            <div class="partner-card">
+                                <div class="partner-card-inner">
+                                    <div class="partner-card-image">
+                                        <img data-src="/assets/images/pages/main/sections/partners/example.svg" class="lazyload" />
+                                    </div>
+                                </div>
+                                <a href="javascript://" target="_blank" class="stretched-link"></a>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 col-xl-4">
+                            <div class="partner-card">
+                                <div class="partner-card-inner">
+                                    <div class="partner-card-image">
+                                        <img data-src="/assets/images/pages/main/sections/partners/example.svg" class="lazyload" />
+                                    </div>
+                                </div>
+                                <a href="javascript://" target="_blank" class="stretched-link"></a>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 col-xl-4">
+                            <div class="partner-card">
+                                <div class="partner-card-inner">
+                                    <div class="partner-card-image">
+                                        <img data-src="/assets/images/pages/main/sections/partners/example.svg" class="lazyload" />
+                                    </div>
+                                </div>
+                                <a href="javascript://" target="_blank" class="stretched-link"></a>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 col-xl-4">
+                            <div class="partner-card">
+                                <div class="partner-card-inner">
+                                    <div class="partner-card-image">
+                                        <img data-src="/assets/images/pages/main/sections/partners/example.svg" class="lazyload" />
+                                    </div>
+                                </div>
+                                <a href="javascript://" target="_blank" class="stretched-link"></a>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 col-xl-4">
+                            <div class="partner-card">
+                                <div class="partner-card-inner">
+                                    <div class="partner-card-image">
+                                        <img data-src="/assets/images/pages/main/sections/partners/example.svg" class="lazyload" />
+                                    </div>
+                                </div>
+                                <a href="javascript://" target="_blank" class="stretched-link"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
