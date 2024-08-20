c36b0c8da9eb1733604137ee3a383670d3c9423c
serj <zenitovic@gmail.com>
2024-08-20 14:44:11 +0300

    update section target

--- resources/views/sections/main/target.blade.php
+++ resources/views/sections/main/target.blade.php
@@ -2,19 +2,19 @@
     <div class="container">
         <div class="section-target-inner">
             <div class="section-target-title">
-                <h4 class="h-4">
+                <h4 class="h-4 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0s">
                     Наши цели
                 </h4>
             </div>
             <div class="section-target-content">
                 <div class="section-target-background">
-                    <img data-src="/assets/images/pages/main/sections/target/bg.svg" class="lazyload" />
+                    <img data-src="/assets/images/pages/main/sections/target/bg.svg" class="lazyload wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.05s" />
                 </div>
                 <div class="section-target-list">
-                    <ul>
-                        <li>Разжигать любовь к чтению у детей и подростков, сделать книги важной частью их жизни;</li>
-                        <li>Поддерживать родителей в создании книжных традиций в семье;</li>
-                        <li>Развивать профессиональные навыки педагогов и представителей культурного сообщества через специализированные программы и тренинги.</li>
+                    <ul class="list">
+                        <li class="list-item wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0s">Разжигать любовь к чтению у детей <br class="d-inline d-xl-none">и подростков, сделать книги <br class="d-none d-xl-inline">важной <br class="d-inline d-xl-none">частью их жизни;</li>
+                        <li class="list-item wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.05s">Поддерживать родителей в создании <br class="d-inline d-xl-none">книжных традиций в семье;</li>
+                        <li class="list-item wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.1s">Развивать профессиональные навыки <br class="d-inline d-xl-none">педагогов и представителей <br class="d-none d-xl-inline">культурного <br class="d-inline d-xl-none">сообщества через специализированные <br class="d-inline d-xl-none">программы <br class="d-none d-xl-inline">и&nbsp;тренинги.</li>
                     </ul>
                 </div>
             </div>
