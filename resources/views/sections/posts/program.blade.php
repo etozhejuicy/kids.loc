db9e9d37d0145f7797b52fe9a9d1aced6d1350d7
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2024-08-21 19:22:07 +0300

    Update program.blade.php

--- resources/views/sections/posts/program.blade.php
+++ resources/views/sections/posts/program.blade.php
@@ -3,7 +3,9 @@
         <div class="section-program-inner">
             <div class="section-program-title text-center text-md-left">
                 <h3 class="h-1 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0s">
-                    Программа фестиваля
+                    Наши мероприятия
+                    <br>
+                    "Название фестиваля"
                 </h3>
             </div>
             <div class="section-program-content">
